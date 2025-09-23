46aa9a992ed4b27871e94dd8100726bbcbe701d1
Marc Haye <marc.antonio.haye@gmail.com>
2025-09-23 21:47:43 +0200

    feat: help msg, args handling

--- bomber.sh
+++ bomber.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-IP="localhost"
-PORT="80"
+# Display help information
+show_help() {
+    cat << EOF
 
-progress_bar() {
+▀██▀▀█▄                      ▀██                      
+ ██   ██    ▄▄▄   ▄▄ ▄▄ ▄▄    ██ ▄▄▄    ▄▄▄▄  ▄▄▄ ▄▄  
+ ██▀▀▀█▄  ▄█  ▀█▄  ██ ██ ██   ██▀  ██ ▄█▄▄▄██  ██▀ ▀▀ 
+ ██    ██ ██   ██  ██ ██ ██   ██    █ ██       ██     
+▄██▄▄▄█▀   ▀█▄▄█▀ ▄██ ██ ██▄  ▀█▄▄▄▀   ▀█▄▄▄▀ ▄██▄    
+                                                
+Usage: $0 [OPTIONS] [SUBDIRECTORY]
+
+OPTIONS:
+    -h, --help         Show this help message
+
+EOF
+}
+
+bombard() {
 
 	# Total number of steps
 	total_steps=20
-
 	# Initial progress
 	progress=0
+	# Delay between each process
+	delay=0.1
 
 	# Display initial state
 	echo -n "[--------------------] 0% "
@@ -21,7 +37,7 @@ progress_bar() {
 	    let filled_slots=progress*20/total_steps
 
 	    # Create the progress bar string
-	    bar=""
+	    bar="Progress: "
 	    for ((i=0; i<$filled_slots; i++)); do
 	        bar="${bar}#"
 	    done
@@ -32,27 +48,43 @@ progress_bar() {
 	    done
 
 	    # Calculate progress percentage
-    	    let percentage=progress*100/total_steps
+    	let percentage=progress*100/total_steps
 
-            # Print progress bar
-            echo -ne "\r[${bar}] ${percentage}% "
+        # Print progress bar
+        echo -ne "\r[${bar}] ${percentage}% "
 
-            # Process
-            bombard;
+        # Process
+        bomb $1;
 
-            # Update progress
-            let progress++
+        # Update progress
+        let progress++
+	    sleep $delay
+	    clear
 	done
 }
 
-bombard() {
-	echo "Bombarding: $IP:$PORT...";
+bomb() {
+	echo "Bombarding: $1...";
 	echo "Press [CTRL+C] to stop.";
-	curl -S -s -o /dev/null $IP:$PORT;
+	curl -S -s -o /dev/null $1;
 }
 
-main() {
-	progress_bar;
-}
+while [[ $# -gt 0 ]]; do
+	case $1 in
+		-h|--help)
+			show_help
+			exit 0
+			;;
+		-*)
+            print_error "Unknown option: $1"
+            show_help
+            exit 1
+            ;;
+        *)
+			ADRESS="$1"
+			shift
+            ;;	
+	esac	
+done
 
-main;
+bombard $ADRESS
